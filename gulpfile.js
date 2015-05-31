46d4043ba71918e233803deb9ac26c844a7f5f82
Stefan Baumgartner <sbaumg@gmail.com>
2015-05-31 17:17:18 +0200

    ch4 ready

--- gulpfile.js
+++ gulpfile.js
@@ -7,26 +7,38 @@ var minifyCSS   = require('gulp-minify-css');
 var prefix      = require('gulp-autoprefixer');
 
 gulp.task('test', function() {
-  return gulp.src(['app/scripts/**/*.js', '!app/scripts/vendor/**/*.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(jshint.reporter('fail'));
+     return gulp.src(['app/scripts/**/*.js', '!app/scripts/vendor/**/*.js'])
+          .pipe(jshint())
+          .pipe(jshint.reporter('default'))
+          .pipe(jshint.reporter('fail'));
 });
 
 
 gulp.task('scripts',
-  gulp.series('test', function scriptsInternal() {
-    return gulp.src(['app/scripts/**/*.js', '!app/scripts/vendor/**/*.js'])
-      .pipe(concat('main.min.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest('dist/scripts'));
-  })
+     gulp.series('test', function scriptsInternal() {
+          return gulp.src(['app/scripts/**/*.js', '!app/scripts/vendor/**/*.js'])
+               .pipe(concat('main.min.js'))
+               .pipe(uglify())
+               .pipe(gulp.dest('dist/scripts'));
+     })
 );
 
 gulp.task('styles', function() {
-  return gulp.src('app/styles/main.less')
-    .pipe(less())
-    .pipe(minifyCSS())
-    .pipe(prefix())
-    .pipe(gulp.dest('dist/styles'));
+     return gulp.src('app/styles/main.less')
+          .pipe(less())
+          .pipe(minifyCSS())
+          .pipe(prefix())
+          .pipe(gulp.dest('dist/styles'));
 });
+
+gulp.task('clean', function(done) {
+     del(['dist'], done);
+});
+
+gulp.task('default', gulp.series('clean', gulp.parallel('styles', 'scripts'), 'server',
+     function watcher(done) {
+          gulp.watch(['app/scripts/**/*.js', '!app/scripts/vendor/**/*.js'], gulp.parallel('scripts'));
+          gulp.watch('app/styles/**/*.less', gulp.parallel('styles'));
+          gulp.watch('dist/**/*', bSync.reload);
+     });
+);
